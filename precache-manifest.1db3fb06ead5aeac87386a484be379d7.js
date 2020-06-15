658de1e9fb7bda737f39320ab5be458b5d03b4cf
Andrew Weiss <wandrew8@gmail.com>
2020-06-15 12:09:59 -0700

    Updates

---
+++ precache-manifest.1db3fb06ead5aeac87386a484be379d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79eb457a99cb798dff50257822a0fd92",
+    "url": "/mapbox/index.html"
+  },
+  {
+    "revision": "2b95a287c0eec389bf11",
+    "url": "/mapbox/static/css/main.00f5f90f.chunk.css"
+  },
+  {
+    "revision": "3a430d8a12baf63bca48",
+    "url": "/mapbox/static/js/2.1f0ecc35.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mapbox/static/js/2.1f0ecc35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b95a287c0eec389bf11",
+    "url": "/mapbox/static/js/main.bd06d85b.chunk.js"
+  },
+  {
+    "revision": "2404885189a5453e12b2",
+    "url": "/mapbox/static/js/runtime-main.e836f124.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mapbox/precache-manifest.4a70a5c8065936dc4f4bc07edd1250bf.js"
+  "/mapbox/precache-manifest.1db3fb06ead5aeac87386a484be379d7.js"
 );
 
 self.addEventListener('message', (event) => {

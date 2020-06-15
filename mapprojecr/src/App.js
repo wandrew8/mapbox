49703dbda33de7c8321da353b113aaf83655363e
Andrew Weiss <wandrew8@gmail.com>
2020-06-15 11:58:34 -0700

    integrates zip code api

--- mapprojecr/src/App.js
+++ mapprojecr/src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import mapboxgl from 'mapbox-gl';
+import FormComponent from './components/Form';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faExpand } from '@fortawesome/free-solid-svg-icons'
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_KEY;
+
+export default class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        lng: -122.34, 
+        lat: 47.61,
+        zoom: 10,
+        city: '',
+        state: '',
+      }
+    }
+
+    getMap = () => {
+      const map = new mapboxgl.Map({
+        container: this.mapContainer,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [this.state.lng, this.state.lat],
+        zoom: this.state.zoom
+        });
+        map.on('move', () => {
+          this.setState({
+          lng: map.getCenter().lng.toFixed(4),
+          lat: map.getCenter().lat.toFixed(4),
+          zoom: map.getZoom().toFixed(2)
+          });
+          });
+          console.log(this.state)
+    }
+
+    componentDidMount() {
+      this.getMap();
+    }
+
+    showFullScreen = () => {
+      this.mapContainer.classList.toggle("fullScreen");
+      this.getMap();
+    }
+
+    getCoordinates = (zipCode) => {
+      fetch(`https://cors-anywhere.herokuapp.com/https://www.zipcodeapi.com/rest/${process.env.REACT_APP_ZIPCODE_API}/info.json/${zipCode}/degrees`, {
+        method: 'GET',
+        headers:{
+          'X-Requested-With': 'XMLHttpRequest'
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          city: data.city,
+          state: data.state,
+          lat: data.lat,
+          lng: data.lng
+        }, () => {
+          this.getMap();
+        })
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+    }
+
+    render() {
+        return (
+          <div className="grid">
+            <FormComponent 
+              getCoordinates={this.getCoordinates}
+            />
+            <div className="mapContainer" ref={el => this.mapContainer = el}>
+            <FontAwesomeIcon icon={faExpand} onClick={this.showFullScreen} className="icon"/>
+            </div>
+              
+          </div>
+        );
+
+    }
 }
-
-export default App;
---
+++ mapprojecr/src/components/Form.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+
+
+export default function FormComponent(props) {
+    const classes = useStyles();
+    const [zipCode, setZipCode] = useState("");
+    
+    const submitForm = (e) => {
+        e.preventDefault();
+        console.log(zipCode)
+        props.getCoordinates(zipCode)
+    }
+
+  return (
+      <div className="formContainer">
+        <form 
+            onSubmit={submitForm}
+            className={classes.root} 
+            autoComplete="off">
+            <TextField 
+            id="outlined-number"
+            label="Zip Code"
+            type="zipcode"
+            value={zipCode}
+            onChange={e => setZipCode(e.target.value)}
+            InputLabelProps={{
+                shrink: true,
+            }}
+            variant="outlined"
+            />
+            <div className="buttonHolder">
+                <Button type="submit" variant="outlined" color="primary">
+                    Find on Map
+                </Button>
+            </div>
+        </form>
+      </div>
+  );
+}
